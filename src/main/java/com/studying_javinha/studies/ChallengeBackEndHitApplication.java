8b21e2bd514eb9b81d69c8197f781affbaa0ac12
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-08-21 21:59:38 -0300

    started project

---
+++ src/main/java/com/studying_javinha/studies/ChallengeBackEndHitApplication.java
@@ -0,0 +1,13 @@
+package com.studying_javinha.studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackEndHitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackEndHitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studying_javinha/studies/ChallengeBackEndHitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studying_javinha.studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackEndHitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
