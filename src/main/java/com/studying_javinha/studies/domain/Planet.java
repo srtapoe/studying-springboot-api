34d4c6cb77ca238148440096a37aef1bcd5e3f97
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-08-27 23:12:07 -0300

    modificated entity Planet

--- src/main/java/com/studying_javinha/studies/domain/Planet.java
+++ src/main/java/com/studying_javinha/studies/domain/Planet.java
@@ -1,10 +1,8 @@
 package com.studying_javinha.studies.domain;
 
+import com.studying_javinha.studies.dto.PlanetDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
@@ -12,16 +10,26 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 @Table(name = "planet")
+@EqualsAndHashCode(of = "id")
 public class Planet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
     private String weather;
     private String land;
 
     @Column(name = "views_on_movies")
-    private int views_on_movies;
+    private int viewsOnMovies;
+
+
+    public Planet(PlanetDTO dto) {
+        this.name = dto.name();
+        this.weather = dto.weather();
+        this.land = dto.land();
+        this.viewsOnMovies = dto.viewsOnMovies();
+    }
 }
