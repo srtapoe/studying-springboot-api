0a378842105025f28b48fd34a8e60e8201c44d56
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-08-27 23:12:30 -0300

    created new exception

---
+++ src/main/java/com/studying_javinha/studies/exceptions/PlanetNotFoundException.java
@@ -0,0 +1,7 @@
+package com.studying_javinha.studies.exceptions;
+
+public class PlanetNotFoundException extends Throwable{
+    public PlanetNotFoundException(String message){
+        super(message);
+    }
+}
